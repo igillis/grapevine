3f67c219d81ee9c8f102efc5610d67a8528396a7
Ian Gillis <igillis@andrew.cmu.edu>
2012-08-28 18:31:09 -0400

    Initial Commit

---
+++ grapevine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  grapevine
+//
+//  Created by Ian Gillis on 8/28/12.
+//  Copyright (c) 2012 Ian Gillis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
