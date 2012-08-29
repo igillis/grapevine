f8cfff2104d0ec354e291d1c65f5eb06db4de70f
Ian Gillis <igillis@andrew.cmu.edu>
2012-08-29 00:09:25 -0400

    Added a login view and made its background purple with a fb login button and login bubbles for our app.

---
+++ grapevine/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  grapevine
+//
+//  Created by Ian Gillis on 8/28/12.
+//  Copyright (c) 2012 Ian Gillis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ grapevine/LoginViewController.m
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.m
+//  grapevine
+//
+//  Created by Ian Gillis on 8/28/12.
+//  Copyright (c) 2012 Ian Gillis. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
