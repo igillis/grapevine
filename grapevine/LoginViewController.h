39fee2dd647f39101bbbfe7a9706c5cefd846fe9
Ian Gillis <hegillis@gmail.com>
2012-10-20 14:48:27 -0400

    removed unimplemented features from login page

--- grapevine/LoginViewController.h
+++ grapevine/LoginViewController.h
@@ -12,17 +12,7 @@
 @interface LoginViewController : UIViewController
 <UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate>
 - (IBAction)facebookLogin:(id)sender;
-
-- (IBAction)twitterLogin:(id)sender;
-
-- (IBAction)grapevineLogin:(id)sender;
-
-- (IBAction)hideKeyboard:(id)sender;
-
 - (void)closeLoginView;
-
-@property (strong, nonatomic) UITextField* username;
-@property (strong, nonatomic) UITextField* password;
 
 @property (strong, nonatomic) IBOutlet UILabel* grapevine;
 
--- grapevine/LoginViewController.m
+++ grapevine/LoginViewController.m
@@ -17,9 +17,6 @@
 @end
 
 @implementation LoginViewController
-
-@synthesize username;
-@synthesize password;
 @synthesize grapevine;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -40,8 +37,6 @@
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    self.username = nil;
-    self.password = nil;
     self.grapevine = nil;
 }
 
@@ -66,78 +61,4 @@
         [self dismissModalViewControllerAnimated:YES];
     }
 }
-
-- (IBAction)twitterLogin:(id)sender {
-    [self dismissModalViewControllerAnimated:YES];
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 2;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-    [tableView setBackgroundColor:[UIColor clearColor]];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                       reuseIdentifier:@"Cell"];
-        cell.accessoryType = UITableViewCellAccessoryNone;
-        
-        UITextField* loginTextField;
-        if ([indexPath row] == 0) {
-            self.username = [[UITextField alloc] initWithFrame:CGRectMake(20, 10, 185, 30)];
-            loginTextField = self.username;
-            loginTextField.placeholder = @"Username";
-            loginTextField.returnKeyType = UIReturnKeyNext;
-        }
-        else {
-            self.password = [[UITextField alloc] initWithFrame:CGRectMake(20, 10, 185, 30)];
-            loginTextField = self.password;
-            loginTextField.placeholder = @"Password";
-            loginTextField.returnKeyType = UIReturnKeyDone;
-            loginTextField.secureTextEntry = YES;
-        }
-        loginTextField.keyboardType = UIKeyboardTypeDefault;
-        loginTextField.adjustsFontSizeToFitWidth = YES;
-        loginTextField.backgroundColor = [UIColor clearColor];
-        loginTextField.autocorrectionType = UITextAutocorrectionTypeNo;
-        loginTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
-        loginTextField.textAlignment = UITextAlignmentLeft;
-        loginTextField.tag = 0;
-        loginTextField.delegate = self;
-        
-        [loginTextField setEnabled: YES];
-        
-        [cell addSubview:loginTextField];
-    }
-    return cell;    
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    
-    return 1;
-}
-
-- (IBAction)grapevineLogin:(id)sender {
-    [self hideKeyboard:nil];
-    [[SessionManager sharedInstance] openGrapevineSessionWithLoginId:username.text andPassword:password.text];
-}
-
-- (IBAction)hideKeyboard:(id)sender {
-    [self.username resignFirstResponder];
-    [self.password resignFirstResponder];
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    if (textField == self.password) {
-        [textField resignFirstResponder];
-        [self grapevineLogin:self];
-    } else if (textField == self.username) {
-        [self.password becomeFirstResponder];
-    }
-    return YES;
-}
-    
 @end
